a5dd747dba16d9cab94147df6387f3f2ded2487c
LaurenSUni <111717811+LaurenSUni@users.noreply.github.com>
2022-08-29 14:32:21 +1000

    80%

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class InputManager : MonoBehaviour
 {
     private Transform[] transArray = new Transform[2];
+  
+    
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,29 @@ public class InputManager : MonoBehaviour
             //rotate blueObj -45 degrees around the z-axis
             transArray[1].transform.Rotate(0, 0, -45);
             
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //redObj and blueObj should swap their vertical positions but keep their horizontal positions
+
+            Debug.Log("red pos1" + transArray[0].transform.position);
+            Debug.Log("blue pos1" + transArray[1].transform.position);
+
+            float redY = transArray[0].transform.position.y;
+            float blueY = transArray[1].transform.position.y;
+
+            float redX = transArray[0].transform.position.x;
+            float redZ = transArray[0].transform.position.z;
+            float blueX = transArray[1].transform.position.x;
+            float blueZ = transArray[1].transform.position.z;
+
+            transArray[0].transform.position = new Vector3(redX, blueY, redZ);
+            transArray[1].transform.position = new Vector3(blueX, redY, blueZ);
+
+            Debug.Log("red pos2" + transArray[0].transform.position);
+            Debug.Log("blue pos2" + transArray[1].transform.position);
+
         }
     }
 }
